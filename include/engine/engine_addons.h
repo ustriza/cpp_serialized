424560d79e5d479380a39601273fcae036360c6f
Yuri Barmin <ustriza@gmail.com>
2026-02-16 21:51:21 +0300

    added writeTimePointDateFormat test

--- include/engine/engine_addons.h
+++ include/engine/engine_addons.h
@@ -43,9 +43,17 @@ bool meta_table_to_cpp(std::chrono::system_clock::time_point &value, const Stora
 namespace yb::from_cpp {
 template<Storage_concept_from_cpp Storage>
 void meta_table_from_cpp(const std::chrono::system_clock::time_point &value, Storage& cur_node) {
-	const auto tvalue = std::chrono::system_clock::to_time_t(value);
-	const auto svalue = yb::string_utils::val_to_string(tvalue);
-	cur_node.interface_assign_from(svalue);
+	const std::string& date_format = cur_node.interface_get_date_format();
+	if (date_format.empty()) {
+		const auto tvalue = std::chrono::system_clock::to_time_t(value);
+		const auto svalue = yb::string_utils::val_to_string(tvalue);
+		
+		cur_node.interface_assign_from(svalue);
+	}
+	else {
+		const std::string svalue = yb::date_formatter::get_string_from(value, date_format);
+		cur_node.interface_assign_from(svalue);
+	}
 }
 	
 }
